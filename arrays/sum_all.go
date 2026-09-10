4ff9d124785a5cf4f19e841f3b6cb28fac89f2d2
„kizwolak” <ki.zwolak@gmail.com>
2026-09-10 23:48:45 +0200

    Add SumAllTails

--- arrays/sum_all.go
+++ arrays/sum_all.go
@@ -1,8 +1,7 @@
 package main
 
 func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
+	var sums []int
 
 	for _, numbers := range numbersToSum {
 		sums = append(sums, Sum(numbers))
